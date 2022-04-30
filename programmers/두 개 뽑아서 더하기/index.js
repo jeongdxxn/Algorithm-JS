41da7e63ecb10f3c968be4d04f535cfdd3b96050
jeongdxxn <dxxn.jeong@gmail.com>
2022-05-01 00:02:19 +0900

    level 1

--- programmers/두 개 뽑아서 더하기/index.js
+++ programmers/두 개 뽑아서 더하기/index.js
@@ -1,20 +1,5 @@
-//! 테스트값 9개 중 2개 통과
-// new Set 으로 중복요소를 삭제할 필요x
-function solution(numbers) {
-  // 이중반복문
-  // numbers 요소에 나머지 요소를 더해준 값을 배열에 넣어주는데
-  // 그때 중복되는 요소가 있을 경우 넣지않는다.
-  let arr = [...new Set(numbers.sort())];
-  let result = [];
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      let sum = numbers[i] + numbers[j];
-      if (!result.includes(sum)) result.push(sum);
-    }
-  }
-  return result;
-}
-
+// numbers 요소에 나머지 요소를 더해준 값을 배열에 넣어주는데
+// 그때 중복되는 요소가 있을 경우 넣지않는다.
 //! 테스트값 9개 중 4개 통과
 function solution(numbers) {
   let sortArr = numbers.sort((a, b) => a - b);
@@ -29,8 +14,7 @@ function solution(numbers) {
 }
 
 //! 통과
-// numbers를 먼저 정렬하고 요소를 더한 값을 출력하는 것과
-// 요소를 더한 값을 출력하고 정렬하는 것의 차이점이 뭔지 알아보기
+// * numbers를 먼저 정렬하고 요소를 더한 값을 출력하는 것과 요소를 더한 값을 출력하고 정렬하는 것의 차이점이 뭔지 알아보기
 function solution(numbers) {
   let result = [];
   for (let i = 0; i < numbers.length; i++) {
