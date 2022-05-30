530cbf6b13e39efac164ea1401b37a97bdbf12b1
jeongdxxn <dxxn.jeong@gmail.com>
2022-05-30 15:18:26 +0900

    level 1

---
+++ programmers/같은 숫자는 싫어/index.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i - 1] !== arr[i]) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
+
+//다른 풀이
+function solution(arr) {
+  return arr.filter((el, index) => el != arr[index + 1]);
+}
