7303af6ea5d1388c7bced4bdf7ba059fd78e0aff
jeongdxxn <dxxn.jeong@gmail.com>
2022-10-07 10:26:43 +0900

    level 1 풀이 추가

--- programmers/문자열 다루기 기본/index.js
+++ programmers/문자열 다루기 기본/index.js
@@ -1,4 +1,14 @@
-function solution(s) {
+function solution1(s) {
   // '==' 는 서로 다른 유형의 두 변수의 값만 비교, 자료형은 판단x
   return (s.length === 4 || s.length === 6) && s == parseInt(s);
+}
+
+/*
+* 정규식 풀이
+*/
+function solution2(s) {
+  // d가 숫자를 뜻하는 표현식이기때문에 d{6}, d{4} 은 숫자 6자리, 4자리를 찾는다.
+  // ^ : 시작, $ : 끝
+  var regex = /^\d{6}$|^\d{4}$/;
+  return regex.test(s);
 }
